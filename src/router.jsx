3ee0076c117843414a1459e97cbbf954996215c1
Victor Aalo <victoraalo@gmail.com>
2025-10-23 23:24:57 +0100

    fixed broken route

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import ProfileCard from "./routes/ProfileCard";
-import ProfileCards from "./routes/gpt";
+
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/profile-card" element={<ProfileCard/>}/>
-                <Route path="/profile-cards" element={<ProfileCards/>}/>
+                 
             </Routes>
         </BrowserRouter>
     )
